3af42929262192c5b094887033d102e39bd5da02
Giorgi Chkhikvadze <giorgichxikvadze972@gmail.com>
2022-07-21 15:01:30 +0400

    Ended First Page

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ const closeSearch = () => {
 }
 
 const burgerMenu = () => {
+    headerTop.style.position = 'relative';
     hamburger.classList.toggle('active');
     burgerMenuShown.classList.toggle('active');
     headerTop.classList.toggle('active');
@@ -21,4 +22,24 @@ dropDownButton.forEach(n => n.addEventListener('click', () => {
     n.parentElement.parentElement.classList.toggle('active');
 }))
 
-console.log(document.body.clientWidth > 768 && headerTop.classList.contains('active'))
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if(prevScrollpos < 40){
+    headerTop.style.background = 'transparent';
+    headerTop.style.top = `0px`;
+    headerTop.style.position = 'relative'
+  }else if (prevScrollpos > currentScrollPos) {
+    headerTop.style.background = '#191428';
+    headerTop.style.top = `0px`;
+    headerTop.style.position = 'fixed'
+    headerTop.style.zIndex = '9999'
+  } else {
+    headerTop.style.background = 'transparent';
+        headerTop.style.top = `-${headerTop.clientHeight}px`;
+        headerTop.style.position = 'relative'
+  }
+  prevScrollpos = currentScrollPos;
+  
+}
